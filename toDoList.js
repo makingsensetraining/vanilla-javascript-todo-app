08316029440e0399cc831bfd7bb6b7149ecd4b37
unknown <lgarcia@Desarrollo43.ms.local>
2017-02-17 17:23:28 -0300

    se cambio for por foreach, se agregaron ';', se traen datos de un json con ajax

--- toDoList.js
+++ toDoList.js
@@ -1,127 +1,132 @@
-var addButtonElement = document.getElementById("addButton");
-var itemDescriptionElement = document.getElementById("ItemDescription");
-var toDoListElement = document.getElementById("toDoList");
+var app = (function(){
+  var addButtonElement = document.getElementById("addButton");
+  var itemDescriptionElement = document.getElementById("ItemDescription");
+  var toDoListElement = document.getElementById("toDoList");
 
-var item = (function() {
-  var id = 0;
+  var item = (function() {
+    var id = 0;
+    return function(description, toggleDone) {
+      this.id = id++;
+      this.description = description;
+      this.checked = toggleDone;
+    };
+  })();
 
-  return function(description) {
-    this.id = id++;
-    this.description = description;
-    this.checked = false;
+  var toDoList = (function() {
+    var itemsList = [];
+    return {
+      loadItems: function() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if(this.readyState == 4 && this.status == 200) {
+            itemsList = JSON.parse(this.responseText);
+            itemsList.forEach(function(element) {
+              toDoList.addItem(element.description, element.checked);
+            });
+          }
+        };
+        xhttp.open("GET", "http://localhost:3000/data/items.json", true);
+        xhttp.send();
+      },
+      addItem: function(description, toggleDone) {
+        //Create new item and add to the model
+        var newItem = new item(description, toggleDone);
+        itemsList.push(newItem);
+        //Add to the view
+        var itemElement = createItemElement(newItem.id, newItem.description, toggleDone);
+        toDoListElement.appendChild(itemElement);
+        //Clear the text-box
+        itemDescriptionElement.value = "";
+      },
+      removeItem: function(id) {
+        var itemSelected = document.getElementById("item" + id);
+        //Remove from the model
+        itemsList.forEach(function(element, index) {
+          if(element.id == id) {
+            itemsList.splice(index, 1);
+          }
+        });
+        //Remove from the view
+        toDoListElement.removeChild(itemSelected);
+      },
+      toggleDone: function(id) {
+        var itemSelectedText = document.getElementById("descriptionText" + id);
+        itemsList.forEach(function(element, index) {
+          if(element.id == id) {
+            if(element.checked) {
+              //Model
+              element.checked = false;
+              //View
+              itemSelectedText.setAttribute("class", "");
+            } else {
+              //Model
+              element.checked = true;
+              //View
+              itemSelectedText.setAttribute("class", "item-selected");
+            }
+          }
+        });
+      }
+    };
+  })();
+
+  //Create the HTML structure for the items
+  function createItemElement(id, desciption, toggleDone) {
+    //<li id="item#">
+    var itemElement = document.createElement("li");
+    itemElement.setAttribute("id", "item" + id);
+    //<div class="div-description">
+    var itemDescriptioncontainerElement = document.createElement("div");
+    itemDescriptioncontainerElement.setAttribute("class", "description");
+    //<span id="descriptiontext#"> Text </span></div>
+    var descriptionText = document.createElement("span");
+    descriptionText.setAttribute("id", "descriptionText" + id);
+    descriptionText.appendChild(document.createTextNode(desciption));
+    itemDescriptioncontainerElement.appendChild(descriptionText);
+    itemElement.appendChild(itemDescriptioncontainerElement);
+    //<div class="div-actions">
+    var actionscontainerElement = document.createElement("div");
+    actionscontainerElement.setAttribute("class", "actions");
+    //<imput type="checkbox">
+    var checkboxElement = document.createElement("input");
+    checkboxElement.setAttribute("type", "checkbox");
+    if(toggleDone) {
+      checkboxElement.checked = true;
+      descriptionText.setAttribute("class", "item-selected");
+    }
+    checkboxElement.addEventListener("click", function(e) {
+      toDoList.toggleDone(id);
+    });
+    actionscontainerElement.appendChild(checkboxElement);
+    //<imput type="button" class="delete-button"></div></li>
+    var deleteButtonElement = document.createElement("button");
+    deleteButtonElement.appendChild(document.createTextNode("DEL"));
+    deleteButtonElement.setAttribute("class", "delete-button");
+    deleteButtonElement.addEventListener("click", function(e) {
+      toDoList.removeItem(id);
+    });
+    actionscontainerElement.appendChild(deleteButtonElement);
+    itemElement.appendChild(actionscontainerElement);
+    return itemElement;
   }
 
-})();
+  toDoList.loadItems();
 
-var toDoList = (function() {
-  var itemsList = [];
-
-  return {
-    addItem: function(description) {
-      //Create new item and add to the model
-      var newItem = new item(description);
-      itemsList.push(newItem);
-      //Add to the view
-      var itemElement = createItemElement(newItem.id, newItem.description);
-      toDoListElement.appendChild(itemElement);
-      //Clear the text-box
-      itemDescriptionElement.value = "";
-    },
-
-    removeItem: function(id) {
-      var itemSelected = document.getElementById("item" + id);
-      //Remove from the model
-      for (var i = 0; i < itemsList.length; i++) {
-        if (itemsList[i].id == id) {
-          itemsList.splice(i, 1);
-        }
-      }
-      //Remove from the view
-      toDoListElement.removeChild(itemSelected);
-    },
-
-    toggleDone: function(id) {
-      var itemSelectedText = document.getElementById("descriptionText" + id);
-
-      for (var i = 0; i < itemsList.length; i++) {
-        if (itemsList[i].id == id) {
-          if (itemsList[i].checked) {
-            //Model
-            itemsList[i].checked = false;
-            //View
-            itemSelectedText.setAttribute("class", "");
-          } else {
-            //Model
-            itemsList[i].checked = true;
-            //View
-            itemSelectedText.setAttribute("class", "item-selected");
-          }
-        }
-      }
+  function sendDescriptionValue() {
+    if(itemDescriptionElement.value.length) {
+      toDoList.addItem(itemDescriptionElement.value, false);
     }
   }
-})();
 
-//Create the HTML structure for the items
-function createItemElement(id, desciption) {
-  //<li id="item#">
-  var itemElement = document.createElement("li");
-  itemElement.setAttribute("id", "item" + id);
+  //Add event to Enter key up
+  itemDescriptionElement.addEventListener("keyup", function(event) {
+    if(event.keyCode == 13) {
+      sendDescriptionValue();
+    }
+  });
 
-  //<div class="div-description">
-  var itemDescriptioncontainerElement = document.createElement("div");
-  itemDescriptioncontainerElement.setAttribute("class", "div-description")
-
-  //<span id="descriptiontext#"> Text </span></div>
-  var descriptionText = document.createElement("span");
-  descriptionText.setAttribute("id", "descriptionText" + id);
-  descriptionText.appendChild(document.createTextNode(desciption));
-  itemDescriptioncontainerElement.appendChild(descriptionText);
-
-  itemElement.appendChild(itemDescriptioncontainerElement);
-
-  //<div class="div-actions">
-  var actionscontainerElement = document.createElement("div");
-  actionscontainerElement.setAttribute("class", "div-actions")
-
-  //<imput type="checkbox">
-  var checkboxElement = document.createElement("input");
-  checkboxElement.setAttribute("type", "checkbox");
-  checkboxElement.addEventListener("click", function(e) {
-    toDoList.toggleDone(id);
-  })
-
-  actionscontainerElement.appendChild(checkboxElement);
-
-  //<imput type="button" class="delete-button"></div></li>
-  var deleteButtonElement = document.createElement("button");
-  deleteButtonElement.appendChild(document.createTextNode("DEL"));
-  deleteButtonElement.setAttribute("class", "delete-button");
-  deleteButtonElement.addEventListener("click", function(e) {
-    toDoList.removeItem(id);
-  })
-
-  actionscontainerElement.appendChild(deleteButtonElement);
-
-  itemElement.appendChild(actionscontainerElement);
-
-  return itemElement;
-}
-
-function sendDescriptionValue() {
-  if (itemDescriptionElement.value.length != 0) {
-    toDoList.addItem(itemDescriptionElement.value);
-  }
-}
-
-//Add event to Enter key up
-itemDescriptionElement.addEventListener("keyup", function(event) {
-  if (event.keyCode == 13) {
+  //Add function for onclick event
+  addButtonElement.onclick = function() {
     sendDescriptionValue();
-  }
-});
-
-//Add function for onclick event
-addButtonElement.onclick = function() {
-  sendDescriptionValue();
-}
+  };
+})();
